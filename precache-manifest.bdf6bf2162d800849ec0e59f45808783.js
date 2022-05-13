50527b7bf365c7c2d71accf607ae44e3c412367e
Bhupendra Singh <singhbhupendra983@gmail.com>
2022-05-13 10:26:16 +0530

    Updates

---
+++ precache-manifest.bdf6bf2162d800849ec0e59f45808783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9e78ab34aca8639b9bb",
+    "url": "/Translator/static/css/main.caeacec5.chunk.css"
+  },
+  {
+    "revision": "b9e78ab34aca8639b9bb",
+    "url": "/Translator/static/js/main.b9e78ab3.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/Translator/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/Translator/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "0cd2ab5fb779b5b7be5b",
+    "url": "/Translator/static/js/runtime~main.0cd2ab5f.js"
+  },
+  {
+    "revision": "78809a3c38df527fa01974420b39ef6a",
+    "url": "/Translator/index.html"
+  }
+];
