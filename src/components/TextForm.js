04c9f881038280080fdf611ffbe6a78073edce32
Bhupendra Singh <singhbhupendra983@gmail.com>
2022-05-13 12:30:44 +0530

    third commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,6 +28,20 @@ export default function TextForm(props) {
         //console.log("On Changed");
         setText(event.target.value);
     }
+    const handleCopy = () =>{
+        //console.log("On Changed");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied on ClipBoard"  ,"Success");
+        
+    }
+    const handleExtraSpaces = () =>{
+       let newText = text.split(/[ ]+/);
+       setText(newText.join(" "));
+       props.showAlert("Extra Space has been removed" ,"Success");
+    }
   const [text ,setText] = useState('');
   return (
     <>
@@ -40,13 +54,15 @@ export default function TextForm(props) {
         <button className ="btn btn-primary"  onClick = {handleUPClick}>Convert to upper case</button>
         <button type="button" className="btn btn-success mx-2"  onClick = {handleLOClick}>Convert to Lower case</button>
         <button type="button" className="btn btn-success mx-1"  onClick = {handleClearClick}>Clear Text</button>
+        <button type="button" className="btn btn-success mx-1"  onClick = {handleCopy}>Copy Clipboard</button>
+        <button type="button" className="btn btn-success mx-1"  onClick = {handleExtraSpaces}>Clear Spaces</button>
         
         
       
     </div>
     <div className='container my-3' style ={{color :props.mode ==='dark' ?'white':'purple'}}>
         
-        <p> <b>Word_count :</b> = { text.split(" ").length -1 }     </p>
+        <p> <b>Word_count :</b> = { text.split(/\s+/).filter((element) =>{return element.length!== 0}).length} </p>
         <p> <b>Charcater_count :</b> {text.length}</p>
         <p><b>Reading_time </b>: {text.length === 0 ? 0:0.008 * text.split(" ").length} ms</p>
 
